0336bf3c1ba2ba36019bb2f33199a53799f5ece9
parkeradam <30392071+parkeradam@users.noreply.github.com>
2022-12-04 11:00:08 +0000

    day 4 solutions

--- 2022/go/main.go
+++ 2022/go/main.go
@@ -4,10 +4,12 @@ import (
 	"github.com/parkeradam/aoc_2022/day_01"
 	"github.com/parkeradam/aoc_2022/day_02"
 	"github.com/parkeradam/aoc_2022/day_03"
+	"github.com/parkeradam/aoc_2022/day_04"
 )
 
 func main() {
 	day_01.Solution()
 	day_02.Solution()
 	day_03.Solution()
+	day_04.Solution()
 }
