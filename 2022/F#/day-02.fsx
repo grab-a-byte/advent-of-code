0a46a10e986dff8e35520bdc880035f0c11a3618
parkeradam <30392071+parkeradam@users.noreply.github.com>
2023-03-07 13:31:04 +0000

    part 2 of day 2

--- 2022/F#/day-02.fsx
+++ 2022/F#/day-02.fsx
@@ -1,5 +1,8 @@
 open System
 
+//TODO Refactor to handle errors instead of raising exceptions
+//TODO Rename functions
+
 let input =
     """A Y
 B X
@@ -8,7 +11,12 @@ C Z"""
 // First is opp
 // Second is you
 
-let (|Rock|Paper|Scissors|) input =
+type Throw =
+    | Rock
+    | Paper
+    | Scissors
+
+let getThrow input =
     match input with
     | 'A'
     | 'X' -> Rock
@@ -38,12 +46,42 @@ let score (a, b) =
     | (Paper, Paper) -> 3
     | (Paper, Scissors) -> 6
 
-let res =
+let pairs =
     input.Split "\n"
     |> Array.map (fun l -> (l[0], l[2]))
+
+let res =
+    pairs
+    |> Array.map (fun (a, b) -> (getThrow a, getThrow b))
     |> Array.map (fun (opp, me) -> (score (opp, me)) + (shapeScore me))
     |> Array.reduce (+)
 
 printfn $"Part 1: {res}"
 
-//TODO Part 2
+let (|Lose|Win|Draw|) input =
+    match input with
+    | 'X' -> Lose
+    | 'Y' -> Draw
+    | 'Z' -> Win
+    | _ -> raise (Exception "Invalid Char")
+
+let calcMe =
+    function
+    | (Rock, Draw) -> Rock
+    | (Rock, Win) -> Paper
+    | (Rock, Lose) -> Scissors
+    | (Scissors, Draw) -> Scissors
+    | (Scissors, Win) -> Rock
+    | (Scissors, Lose) -> Paper
+    | (Paper, Draw) -> Paper
+    | (Paper, Win) -> Scissors
+    | (Paper, Lose) -> Rock
+
+let calcRound (opp, me) =
+    let oppThrow = getThrow opp
+    let myThrow = calcMe (oppThrow, me)
+    (score (oppThrow, myThrow)) + (shapeScore myThrow)
+
+let res2 = pairs |> Array.map calcRound |> Array.reduce (+)
+
+printfn $"Part 2: {res2}"
