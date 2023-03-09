9e52771855b6a1e323474a2c38ad20b741f30fbf
parkeradam <30392071+parkeradam@users.noreply.github.com>
2023-03-09 17:06:18 +0000

    day06

--- 2022/F#/day-06.fsx
+++ 2022/F#/day-06.fsx
@@ -12,7 +12,7 @@ let getMarker windowSize (input: string) =
     |> Array.mapi (fun i x -> (i, charsAreUnique x))
     |> Array.filter (fun (i, b) -> b)
     |> Array.head
-    |> fun (index, _) -> index + 4
+    |> fun (index, _) -> index + windowSize
 
 printfn "Part 1 Test 1: %i" (getMarker 4 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
 printfn "Part 1 Test 2: %i" (getMarker 4 "bvwbjplbgvbhsrlpgdmjqwftvncz")
@@ -20,6 +20,16 @@ printfn "Part 1 Test 3: %i" (getMarker 4 "nppdvjthqldpwncqszvftbrmjlhg")
 printfn "Part 1 Test 4: %i" (getMarker 4 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
 printfn "Part 1 Test 5: %i" (getMarker 4 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
 
-let res1 =
-    File.ReadAllText "2022/F#/day-06.txt"
-    |> getMarker 4
+let res1 = File.ReadAllText "2022/F#/day-06.txt" |> getMarker 4
+
+printfn $"Part 1 : {res1}"
+
+// Part 2
+printfn "Part 2 Test 1: %i" (getMarker 14 "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+printfn "Part 2 Test 2: %i" (getMarker 14 "bvwbjplbgvbhsrlpgdmjqwftvncz")
+printfn "Part 2 Test 3: %i" (getMarker 14 "nppdvjthqldpwncqszvftbrmjlhg")
+printfn "Part 2 Test 4: %i" (getMarker 14 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+printfn "Part 2 Test 5: %i" (getMarker 14 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+
+let res2 = File.ReadAllText "2022/F#/day-06.txt" |> getMarker 14
+printfn $"Part 2 : {res2}"
