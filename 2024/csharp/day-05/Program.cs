952b25eec3ef892d56ebc326d915f150ed237fca
grab-a-byte <30392071+grab-a-byte@users.noreply.github.com>
2024-12-05 20:36:44 +0000

    Part 2 attempt

--- 2024/csharp/day-05/Program.cs
+++ 2024/csharp/day-05/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Data;
 
-var lines = File.ReadAllLines("./test.txt");
+var lines = File.ReadAllLines("./input.txt");
 var orderedRules = lines.TakeWhile((x) => !string.IsNullOrWhiteSpace(x));
 var input = lines.Skip(orderedRules.Count() + 1);
 
@@ -26,20 +26,7 @@ List<List<int>> invalidRows = [];
 foreach (var row in input)
 {
     var parts = row.Split(',').Select(int.Parse).ToList();
-    var correct = true;
-    for (int i = 0; i < parts.Count; i++)
-    {
-        var before = parts.Take(i);
-        if (rules.TryGetValue(parts[i], out var checks))
-        {
-            if (!before.All(x => !checks.Contains(x)))
-            {
-                correct = false;
-                break;
-            }
-        }
-    }
-    if (correct)
+    if (ValidateRow(parts))
     {
         validRows.Add(parts);
     }
@@ -53,3 +40,46 @@ var result = validRows.Select(x => x[x.Count / 2]).Sum();
 Console.WriteLine($"Day Five - Part 1: {result}");
 
 //TODO: Part 2
+List<List<int>> fixedRows= [];
+foreach(var badRow in invalidRows){
+    var newRow = badRow;
+    while(!ValidateRow(newRow)){
+    newRow.Sort(delegate(int x, int y)
+        {
+            if(rules.TryGetValue(x, out var check)){
+                if(check.Contains(y)) return -1;
+                if(!check.Contains(y)) return 0;
+            }
+            return 1;
+        });
+    }
+    fixedRows.Add(newRow);
+}
+
+foreach(var x in fixedRows){
+        Console.Write('[');
+        foreach(var y in x){
+            Console.Write($"{y},");
+        }
+        Console.Write(']');
+        Console.WriteLine();
+}
+
+var result2 = fixedRows.Select(x => x[x.Count / 2]).Sum();
+Console.WriteLine($"Day Five - Part 2: {result2}");
+
+
+bool ValidateRow(List<int> parts){
+    for (int i = 0; i < parts.Count; i++)
+    {
+        var before = parts.Take(i);
+        if (rules.TryGetValue(parts[i], out var checks))
+        {
+            if (!before.All(x => !checks.Contains(x)))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
